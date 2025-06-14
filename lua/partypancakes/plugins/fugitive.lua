4586eb98e58d6fd8c33557cbb43a8c3c51111d77
Bilge Onayci <bilgehalilonayci@gmail.com>
2025-06-14 14:18:42 +0300

    add git pull

--- lua/partypancakes/plugins/fugitive.lua
+++ lua/partypancakes/plugins/fugitive.lua
@@ -1,6 +1,10 @@
 return {
     "tpope/vim-fugitive",
     config = function()
-        vim.keymap.set("n", "<leader>Gs", vim.cmd.Git, { desc = "Git Status"})
+        vim.keymap.set("n", "<leader>Gs", vim.cmd.Git, { desc = "Git Status" })
+        vim.keymap.set("n", "<leader>Gp", function()
+            local branch = vim.fn.system("git rev-parse --abbrev-ref HEAD"):gsub("%s+", "")
+            vim.cmd.Git("pull origin " .. branch)
+        end, { desc = "Git pull" })
     end
 }
