3058de6a2f48a66efc110a2fabc64ff0f887bb6f
Bilge Onayci <bilgehalilonayci@gmail.com>
2025-07-05 22:07:52 +0300

    snippet keymap

--- lua/partypancakes/plugins/snippets.lua
+++ lua/partypancakes/plugins/snippets.lua
@@ -1,45 +1,33 @@
 return {
-	{
-		"L3MON4D3/LuaSnip",
-		-- follow latest release.
-		version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-		build = "make install_jsregexp",
+    {
+        "L3MON4D3/LuaSnip",
+        -- follow latest release.
+        version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+        build = "make install_jsregexp",
 
-		dependencies = { "rafamadriz/friendly-snippets" },
+        dependencies = { "rafamadriz/friendly-snippets" },
 
-		config = function()
-			local ls = require("luasnip")
-			ls.config.set_config({
-				history = true,
-				updateevents = "TextChanged,TextChangedI",
-			})
+        config = function()
+            local ls = require("luasnip")
+            ls.config.set_config({
+                history = true,
+                updateevents = "TextChanged,TextChangedI",
+            })
 
-			-- load custom snippets
-			require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/resources/snippets" })
-			require("luasnip").config.setup({
-				update_events = "TextChanged,TextChangedI",
-				enable_autosnippets = true,
-			})
+            -- load custom snippets
+            require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/resources/snippets" })
+            require("luasnip").config.setup({
+                update_events = "TextChanged,TextChangedI",
+                enable_autosnippets = true,
+            })
 
-			-- local ls = require("luasnip")
-			-- ls.filetype_extend("javascript", { "jsdoc" })
-			--
-			-- vim.keymap.set({ "i" }, "<C-s>e", function()
-			-- 	ls.expand()
-			-- end, { silent = true })
-			--
-			-- vim.keymap.set({ "i", "s" }, "<C-s>;", function()
-			-- 	ls.jump(1)
-			-- end, { silent = true })
-			-- vim.keymap.set({ "i", "s" }, "<C-s>,", function()
-			-- 	ls.jump(-1)
-			-- end, { silent = true })
-			--
-			-- vim.keymap.set({ "i", "s" }, "<C-E>", function()
-			-- 	if ls.choice_active() then
-			-- 		ls.change_choice(1)
-			-- 	end
-			-- end, { silent = true })
-		end,
-	},
+            vim.keymap.set({ "i", "s" }, "<Tab>", function()
+                if ls.locally_jumpable(1) then
+                    return "<cmd>lua require'luasnip'.jump(1)<CR>"
+                else
+                    return "<Tab>"
+                end
+            end, { expr = true, silent = true })
+        end,
+    },
 }
